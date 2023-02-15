10bbda3799f96edad2e01515bdbceac216977891
MrCronkite <vladshima99@gmail.com>
2023-02-15 20:44:34 +0300

    add sprite

--- GameTutorial/GameTutorial/GameScene.swift
+++ GameTutorial/GameTutorial/GameScene.swift
@@ -13,10 +13,24 @@ class GameScene: SKScene {
     private var currentNode: SKNode?
     
     override func didMove(to view: SKView) {
+        if let grid = Grid(blockSize: 70.0, rows:5, cols:5) {
+            grid.position = CGPoint (x:frame.midX, y:frame.midY)
+            addChild(grid)
+            
+            let gamePiece = SKSpriteNode(imageNamed: "Spaceship")
+            gamePiece.setScale(0.0625)
+            gamePiece.position = grid.gridPosition(row: 1, col: 0)
+            grid.addChild(gamePiece)
+        }
+        
         let node = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
         node.name = "draggable"
         
         self.addChild(node)
+        
+        let blueNode = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
+        blueNode.name = "draggable"
+        self.addChild(blueNode)
     }
     
     
@@ -46,6 +60,14 @@ class GameScene: SKScene {
             let touchLocation = touch.location(in: self)
             node.position = touchLocation
         }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.currentNode = nil
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.currentNode = nil
     }
     
     
