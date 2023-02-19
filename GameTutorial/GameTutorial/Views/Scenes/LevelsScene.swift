5691dae7a2e61c6296172a1dfb334df8deb972fa
MrCronkite <vladshima99@gmail.com>
2023-02-20 00:06:30 +0300

    add sprite

--- GameTutorial/GameTutorial/Views/Scenes/LevelsScene.swift
+++ GameTutorial/GameTutorial/Views/Scenes/LevelsScene.swift
@@ -13,19 +13,24 @@ class LevelsScene: SKScene{
     
     var boardCamera = SKCameraNode()
     var background = SKSpriteNode(imageNamed: "bgImage")
+    var scrolView: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        let bounds = UIScreen.main.bounds
+        
+        scrolView = SKSpriteNode(color: UIColor(cgColor: CGColor(red: 255, green: 255, blue: 255, alpha: 0.7)), size: CGSize(width: bounds.size.width / 1.7, height: bounds.size.height))
+        scrolView.position = CGPoint(x: 0, y:  -(bounds.size.height / 2))
+        
         
         boardCamera = self.childNode(withName: "boardCamera") as! SKCameraNode
         
-        let bounds = UIScreen.main.bounds
+    
         background.size = CGSize(width: bounds.size.width, height: bounds.size.height)
         background.zPosition = -4
         background.position = CGPoint(x: 0, y: 0)
         
-        
-        
         addChild(background)
+        boardCamera.addChild(scrolView)
         
     }
     
